a41aed369a9bc0d5f658d502f112f3762e999df1
dairof7 <dairof7@gmail.com>
2020-09-14 15:48:36 -0500

    fix and rewrite with another function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,11 +18,16 @@ def get_user(id):
     data = {'Id': id}
     url = 'https://jsonplaceholder.typicode.com/'
     todos = requests.get(url + 'todos', params={'userId': id}).json()
+    return([name, todos])
+
+def show(data):
+    name = data[0]
+    todos = data[1]
     n = 0
     str_to_print = ''
     for task in todos:
         if task['completed'] is True:
-            num_complete += 1
+            n += 1
             str_to_print += '\t' + task['title'] + '\n'
     print('Employee {} is done with tasks({}/{}):'.format(name, n, len(todos)))
     print(str_to_print, end='')
@@ -30,3 +35,4 @@ def get_user(id):
 
 if __name__ == '__main__':
     data = get_user(argv[1])
+    show(data)
