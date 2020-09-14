d09998a1c514dce7fdb0562bcf3f51ea1c876407
dairof7 <dairof7@gmail.com>
2020-09-14 16:16:32 -0500

    add task #1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""[task 1, get and save on csv]
+"""
+import requests
+from sys import argv
+import csv
+
+
+def get_user(id):
+    """get the user
+    Args:
+        id (integer: user id]
+    """
+    data = {'Id': id}
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users', params={'id': id}).json()
+    data = {'Id': id}
+    url = 'https://jsonplaceholder.typicode.com/'
+    todos = requests.get(url + 'todos', params={'userId': id}).json()
+    return([users, todos])
+
+
+def store_csv(data):
+    users = data[0]
+    todos = data[1]
+    username = users[0]['username']
+    with open(argv[1] + '.csv', 'w') as f:
+        towrite = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            towrite.writerow([task['userId'], username,
+                             task['completed'], task['title']])
+
+
+if __name__ == '__main__':
+    data = get_user(argv[1])
+    store_csv(data)
