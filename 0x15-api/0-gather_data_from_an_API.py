cfb976fd2835f184538f8630127263d9a3093c3d
dairof7 <dairof7@gmail.com>
2020-09-14 15:05:26 -0500

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,30 +5,28 @@
 import requests
 from sys import argv
 
+
 def get_user(id):
     """get the user
     Args:
         id (integer: user id]
-    """    
+    """
     data = {'Id': id}
     url = 'https://jsonplaceholder.typicode.com/'
-    users = requests.get(url + 'users', params={'id':id}).json()
+    users = requests.get(url + 'users', params={'id': id}).json()
     name = users[0]['name']
-    
     data = {'Id': id}
     url = 'https://jsonplaceholder.typicode.com/'
     todos = requests.get(url + 'todos', params={'userId': id}).json()
-    
-    num_complete = 0
+    n = 0
     str_to_print = ''
     for task in todos:
         if task['completed'] is True:
             num_complete += 1
-            str_to_print += '\t'+ task['title'] + '\n'
-    print('Employee {} is done with tasks({}/{}):'.format(name, num_complete, len(todos)))
-    print(str_to_print,end='')
+            str_to_print += '\t' + task['title'] + '\n'
+    print('Employee {} is done with tasks({}/{}):'.format(name, n, len(todos)))
+    print(str_to_print, end='')
+
 
 if __name__ == '__main__':
     data = get_user(argv[1])
-    #print(data)
-    
