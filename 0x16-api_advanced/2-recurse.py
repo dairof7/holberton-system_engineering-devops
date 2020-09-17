329452d0f2bf030e07d8b6fdb359b1a159a76056
dairof7 <dairof7@gmail.com>
2020-09-17 14:33:59 -0500

    add #2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""[task 2, all the hot post]
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """[print 100 hot post]
+
+    Args:
+        subreddit ([string]): [type of subreddit to consult]
+        hot_list (list): [list of the titles]. Defaults to [].
+    """
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    info = requests.get(url, headers={"User-Agent": "dairof7"},
+                        allow_redirects=False,
+                        params={'after': after})
+    after = info.json()['data']['after']
+
+    if info.status_code == 200:
+        if after is None:
+            return hot_list
+
+        for post in info.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        return recurse(subreddit, hot_list, after)
+
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    pass
