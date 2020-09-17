74f6c290f2eb8c220f11d65c3863459049dd0f58
dairof7 <dairof7@gmail.com>
2020-09-17 12:36:06 -0500

    add #1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""[task 0, get rest api]
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """print the top 10 hot post
+
+    Args:
+        subreddit ([string]): [type of subreddit to consult]
+
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    about_info = requests.get(url, headers={"User-Agent": "dairof7"},
+                              allow_redirects=False)
+
+    if about_info.status_code == 200:
+        for post in about_info.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    pass
